3f3f8d7b11ed9bc27374b384cf16f0b85165d5f0
Peter Meilstrup <peter.meilstrup@gmail.com>
2012-10-28 22:23:33 -0700

    recursive functions

--- recursive.erl
+++ recursive.erl
@@ -1,5 +1,5 @@
 -module(recursive).
--export([fac/1, len/1, tail_fac/1, tail_len/1]).
+-export([fac/1, len/1, tail_fac/1, tail_len/1, duplicate/2, tail_duplicate/2]).
 
 fac(0) -> 1;
 fac(N) when N > 0 -> N*fac(N-1).
@@ -14,4 +14,19 @@ tail_fac(N,Acc) when N > 0 -> tail_fac(N-1, N*Acc).
 tail_len(L) -> tail_len(L,0).
 tail_len([],Acc) -> Acc;
 tail_len([_|T], Acc) -> tail_len(T, Acc+1).
+
+duplicate(0,_) ->
+    [];
+duplicate(N, Term) when N > 0 ->
+    [Term|duplicate(N-1,Term)].
+
+
+tail_duplicate(N, Term) ->
+    tail_duplicate(N, Term, []).
+
+tail_duplicate(0,_,List) ->
+    List;
+tail_duplicate(N, Term, List) when N > 0 ->
+    tail_duplicate(N-1, Term, [Term|List]).
+
 
