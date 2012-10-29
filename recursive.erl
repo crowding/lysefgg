99c25c3ab88ad87197ce0b3ce7666fe19ecdf93a
Peter Meilstrup <peter.meilstrup@gmail.com>
2012-10-28 18:22:58 -0700

    tail recursion

--- recursive.erl
+++ recursive.erl
@@ -1,8 +1,17 @@
 -module(recursive).
--export([fac/1]).
+-export([fac/1, len/1, tail_fac/1, tail_len/1]).
 
-%why do they always with such silly factorials.
-%at least it seems to memoize?
-fac(N) when N == 0 -> 1;
+fac(0) -> 1;
 fac(N) when N > 0 -> N*fac(N-1).
+
+len([]) -> 0;
+len([_|T]) -> 1 + len(T).
+
+tail_fac(N) -> tail_fac(N,1).
+tail_fac(0,Acc) -> Acc;
+tail_fac(N,Acc) when N > 0 -> tail_fac(N-1, N*Acc).
+
+tail_len(L) -> tail_len(L,0).
+tail_len([],Acc) -> Acc;
+tail_len([_|T], Acc) -> tail_len(T, Acc+1).
 
