43d130af753f70c8f31048166fc223d2fc348280
Peter Meilstrup <peter.meilstrup@gmail.com>
2012-10-28 22:55:49 -0700

    tail recursive zips

--- recursive.erl
+++ recursive.erl
@@ -1,5 +1,11 @@
 -module(recursive).
--export([fac/1, len/1, tail_fac/1, tail_len/1, duplicate/2, tail_duplicate/2]).
+-export([fac/1, len/1, tail_fac/1, tail_len/1, 
+         duplicate/2, tail_duplicate/2, 
+         reverse/1, tail_reverse/1,
+         sublist/2, tail_sublist/2,
+         zip/2, lenient_zip/2,
+         tail_zip/2, tail_lenient_zip/2
+        ]).
 
 fac(0) -> 1;
 fac(N) when N > 0 -> N*fac(N-1).
@@ -28,5 +34,47 @@ tail_duplicate(0,_,List) ->
     List;
 tail_duplicate(N, Term, List) when N > 0 ->
     tail_duplicate(N-1, Term, [Term|List]).
+
+reverse([]) -> [];
+reverse([H|T]) -> reverse(T)++[H].
+
+tail_reverse(L) -> tail_reverse(L,[]).
+tail_reverse([],Acc) -> Acc;
+tail_reverse([H|T],Acc) -> tail_reverse(T, [H|Acc]).
+
+sublist(_,0) ->
+    [];
+sublist([],_) -> 
+    [];
+sublist([H|T], N) when N > 0 -> 
+    [H|sublist(T,N-1)].
+
+tail_sublist(L,N) -> reverse(tail_sublist(L,N,[])).
+tail_sublist(_,0,SubList) -> SubList;
+tail_sublist([],_,SubList) -> SubList;
+tail_sublist([H|T], N, SubList) when N > 0 -> 
+tail_sublist(T, N-1, [H|SubList]).
+
+zip([], []) -> [];
+zip([X|Xs],[Y|Ys]) -> [{X,Y}|zip(Xs,Ys)].
+
+lenient_zip([],_) -> [];
+lenient_zip(_,[]) -> [];
+lenient_zip([X|Xs],[Y|Ys]) -> [{X,Y}|lenient_zip(Xs,Ys)].
+
+tail_zip(X, Y) -> reverse(tail_zip(X,Y,[])).
+
+tail_zip([], [], Acc) -> Acc;
+tail_zip([X|Xs], [Y|Ys], Acc) -> tail_zip(Xs,Ys,[{X,Y}|Acc]).
+
+tail_lenient_zip(X,Y) ->
+    reverse(tail_lenient_zip(X,Y,[])).
+
+tail_lenient_zip([],_,Acc) -> Acc;
+tail_lenient_zip(_,[],Acc) -> Acc;
+tail_lenient_zip([X|Xs],[Y|Ys], Acc) ->
+    tail_lenient_zip(Xs, Ys, [{X,Y}|Acc]).
+
+
 
 
