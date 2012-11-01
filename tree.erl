eb55e71e572416bc41d7cbe36c030e910ecc9e58
Peter Meilstrup <peter.meilstrup@gmail.com>
2012-10-31 23:46:42 -0700

    tree lookup using throw

--- tree.erl
+++ tree.erl
@@ -1,5 +1,6 @@
 -module(tree).
--export([empty/0, insert/3, lookup/2]).
+-export([empty/0, insert/3, lookup/2,
+         has_value/2, has_value_throwing/2]).
 
 empty() -> {node, 'nil'}.
 
@@ -22,3 +23,37 @@ lookup(Key, {node, {NodeKey, _, Smaller, _}}) when Key < NodeKey ->
     lookup(Key, Smaller);
 lookup(Key, {node, {NodeKey, _, _, Larger}}) when Key > NodeKey ->
     lookup(Key, Larger).
+
+has_value(_, {node, 'nil'}) ->
+    false;
+has_value(Val, {node, {_, Val, _, _}}) ->
+    true;
+has_value(Val, {node, {_, _, Left, Right}}) ->
+    case has_value(Val, Left) of
+        true -> true;
+        false -> has_value(Val, Right)
+    end.
+
+has_value_throwing(Val, Tree) ->
+    try has_value1(Val, Tree) of
+        false -> false
+    catch
+        true -> true
+    end.
+
+has_value1(_, {node, 'nil'}) ->
+    false;
+has_value1(Val, {node, {_, Val, _, _}}) ->
+    throw(true);
+has_value1(Val, {node, {_, _, Left, Right}}) ->
+    has_value1(Val, Left),
+    has_value1(Val, Right).
+
+% example of populating a tree:
+%
+%Keys -> [s, f, e, j, c, d, r, c, g, d, w].
+%Values -> [1, 2, 4, 6, 4, 8, 3, 4, 2, 6, 73].
+%
+%Tree = lists:foldr(fun({A,B}, Tree) -> tree:insert(A,B, Tree) end,
+%                   tree:empty(),
+%                   lists:zip(tree:keys(), tree:values())).
