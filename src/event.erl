8d1028a6cb6dadccf157c15576aa3aa9aade6731
Peter Meilstrup <peter.meilstrup@gmail.com>
2012-11-04 00:08:30 -0700

    start() interface

--- src/event.erl
+++ src/event.erl
@@ -3,6 +3,30 @@
 
 -record(state, {server, name="", to_go=0}).
 
+start(EventName, Delay) ->
+    spawn(?MODULE, init, [self(), EventName, Delay]).
+
+start_link(EventName, Delay) ->
+    spawn_link(?MODULE, init, [self(), EventName, Delay]).
+
+cancel(Pid) ->
+    %%Monitor in case the process is already dead
+    Ref = erlang:monitor(process, Pid),
+    Pid = {self(), Ref, cancel},
+    receive
+        {Ref, ok} ->
+            erlang:demonitor(Ref, [flush]),
+            ok;
+        {'DOWN', Ref, process, Pid, _reason} ->
+            ok
+    end.
+
+%% Event's innards
+init(Server, EventName, Delay) ->
+    loop(#state{server=Server,
+                name=EventName,
+                to_go=normalize(Delay)}).
+
 loop(S = #state{server=Server, to_go=[T|Next]}) ->
     receive
         {Server, Ref, cancel} ->
@@ -19,19 +43,27 @@ normalize(N) ->
     Limit = 49*24*60*60
         , [N rem Limit | lists:duplicate(N div Limit, Limit)].
 
+time_to_go(TimeOut={{_,_,_}, {_,_,_}}) ->
+    Now = calendar:local_time(),
+    ToGo = calendar:datetime_to_gregorian_seconds(TimeOut)
+           - calendar:datetime_to_gregorian_seconds(Now),
+    Secs = if ToGo > 0 ->
+                   ToGo;
+              ToGo =< 0 -> 0
+           end,
+    normalize(Secs).
+
 test() ->
-    spawn(event, loop, [#state{server=self(), name="test", to_go=normalize(1)}])
+    start("test", 1)
         , timer:sleep(500)
         , flush()
-        , timer:sleep(1000)
+        , timer:sleep(500)
         , flush()
-        , Pid = spawn(event, loop,
-                      [#state{server=self(), name="test", to_go=normalize(500)}])
+        , Pid = start("test", 500)
         , ReplyRef = make_ref()
         , Pid ! {self(), ReplyRef, cancel}
         , flush()
-        , Pid2 = spawn(event, loop, [#state{server=self(), name="test",
-                                            to_go=normalize(365*24*60*60)}])
+        , Pid2 = start("test", 365*24*60*60)
         , ReplyRef2 = make_ref()
         , Pid2 ! {self(), ReplyRef2, cancel}
         , flush()
