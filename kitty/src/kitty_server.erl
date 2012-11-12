2ac8c44278baffc723de2095c21554943e497a8e
Peter Meilstrup <peter.meilstrup@gmail.com>
2012-11-11 19:07:55 -0800

    factor out call

--- kitty/src/kitty_server.erl
+++ kitty/src/kitty_server.erl
@@ -4,23 +4,13 @@
 -export([start_link/0, order_cat/4, return_cat/2, close_shop/1]).
 
 -include("include/cat.hrl").
+
 %%% Client API
 start_link() -> spawn_link(fun init/0).
 
 %% Synchronous call
 order_cat(Pid, Name, Color, Description) ->
-    Ref = erlang:monitor(process, Pid),
-    Pid ! {self(), Ref, {order, Name, Color, Description}},
-    receive
-        {Ref, Cat} ->
-            erlang:demonitor(Ref, [flush]),
-            Cat;
-        {'DOWN', Ref, process, Pid, Reason} ->
-            erlang:error(Reason)
-    after 5000 ->
-            erlang:error(timeout)
-    end.
-
+    my_server:call(Pid, {order, Name, Color, Description}).
 
 %% This call is asynchronous
 return_cat(Pid, Cat = #cat{}) ->
@@ -29,17 +19,7 @@ return_cat(Pid, Cat = #cat{}) ->
 
 %% Synchronous call
 close_shop(Pid) ->
-    Ref = erlang:monitor(process, Pid),
-    Pid ! {self(), Ref, terminate},
-    receive
-        {Ref, ok} ->
-            erlang:demonitor(Ref, [flush]),
-            ok;
-        {'DOWN', Ref, process, Pid, Reason} ->
-            erlang:error(Reason)
-    after 5000 ->
-            erlang:error(timeout)
-    end.
+    my_server:call(Pid, terminate).
 
 %%% Server functions
 init() -> loop([]).
